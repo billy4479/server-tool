5a4e13542309bc9b5e1abcb9f6f24be9eced4d06
billy4479 <giachi.ellero@gmail.com>
2024-04-02 16:27:44 +0200

    Fix zenity!

--- package.nix
+++ package.nix
@@ -9,8 +9,9 @@ in
   buildGoModule rec {
     inherit pname version;
     src = lib.cleanSource ./.;
+    nativeBuildInputs = [pkgs.makeWrapper];
 
-    ldflags = ["-X github.com/billy4479/server-tool/lib.Version=${version}"];
+    ldflags = ["-s" "-w" "-X 'github.com/billy4479/server-tool/lib.Version=${version}'"];
 
     vendorHash = "sha256-YoguesTG55+Cl5ieCF3FFQK3B6EMpjGNmEV8QHu1VKE=";
 
@@ -22,11 +23,10 @@ in
       install -Dm755 ${pname} $out/bin/${pname}
     '';
 
-    postInstall = ''
+    postFixup = ''
       wrapProgram $out/bin/${pname} \
                   --prefix PATH : ${lib.makeBinPath [pkgs.gnome.zenity]}
     '';
-
     meta = with lib; {
       description = "A tool to manage Minecraft servers";
       homepage = "https://github.com/billy4479/server-tool";
